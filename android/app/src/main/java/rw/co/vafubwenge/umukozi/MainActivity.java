bc21b12a39ce6a3710e9d8ae8d0fdf046c55761a
map-boy <map-boy@users.noreply.github.com>
2026-08-17 18:45:13 +0200

    Add Capacitor android/ios projects

---
+++ android/app/src/main/java/rw/co/vafubwenge/umukozi/MainActivity.java
@@ -0,0 +1,5 @@
+package rw.co.vafubwenge.umukozi;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rw.co.vafubwenge.umukozi',
+  appName: 'umukozi',
+  webDir: 'dist'
+};
+
+export default config;
